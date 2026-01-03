b1dad0b45bf66230c02fb9c0cad9a3d201d05fa1
Miguel Briceno <miguel.briceno@nter.es>
2026-01-03 21:13:55 +0100

    chore(config): setup global layout, fonts, and tailwind theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			animation: {
-        typewriter: "typewriter 2s steps(11) forwards"
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      animation: {
+        typewriter: "typewriter 2s steps(11) forwards",
+        incline: 'incline 2s infinite linear',
+        marquee: 'marquee 30s linear infinite',
       },
       keyframes: {
         typewriter: {
@@ -17,10 +19,10 @@ export default {
           '50%': { transform: 'translateX(20px) translateY(-10px)' },
           '100%': { transform: 'translateX(0) translateY(0)' },
         },
-      },
-      animation: {
-        incline: 'incline 2s infinite linear',
-        incline: 'incline 2s infinite linear',
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-50%)' },
+        },
       },
       dropShadow: {
         '3xl': '0 0 20px rgba(42, 193, 126 , 0.35)',
@@ -29,8 +31,7 @@ export default {
             '0 45px 65px rgba(0, 0, 0, 0.15)'
         ]
       }
-		},
-		theme: "dark"
-	},
-	plugins: [],
+    },
+  },
+  plugins: [],
 }
